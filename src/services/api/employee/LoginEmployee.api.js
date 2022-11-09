66b1b92db80af87f429e600e87ceb1d23cd1675b
maryam <m.safari.anarkuli@gmail.com>
2022-11-09 15:16:49 +0330

    env fix

--- src/services/api/employee/LoginEmployee.api.js
+++ src/services/api/employee/LoginEmployee.api.js
@@ -1,9 +1,10 @@
-import http from "../../Interceptor/Interceptor";
-import * as Storage from "../../Storage/Storage";
-import toast from "react-hot-toast";
+import http from '../../Interceptor/Interceptor';
+import * as Storage from '../../storage/Storage';
+import toast from 'react-hot-toast';
 
 //Main Url Of Our Project Backend
-const MainURL = process.env.REACT_APP_PUBLIC_URL;
+const MainURL = process.env.REACT_APP_PUBLIC_API_URL;
+console.log(MainURL);
 
 export const LoginEmployee = async (object) => {
   try {
@@ -25,11 +26,11 @@ export const LoginEmployee = async (object) => {
 
     //Set The Employee Token & Employee Information In Local Storage Or Session Storage
 
-    Storage.setItem("token", employeeToken);
-    Storage.setItem("userInfo", JSON.stringify(employeeInfo));
+    Storage.setItem('token', employeeToken);
+    Storage.setItem('userInfo', JSON.stringify(employeeInfo));
 
-    Storage.sessionSetItem("token", employeeToken);
-    Storage.sessionSetItem("userInfo", JSON.stringify(employeeInfo));
+    Storage.sessionSetItem('token', employeeToken);
+    Storage.sessionSetItem('userInfo', JSON.stringify(employeeInfo));
 
     //Condition For Displaying Toast Massage If The Login Is Seccessfull
     if ((result.data.success = true))
@@ -37,7 +38,7 @@ export const LoginEmployee = async (object) => {
 
     //Redirect The Employee To Admin Pannel Page
     setTimeout(() => {
-      window.location = "/home";
+      window.location = '/home';
     }, 2000);
     return result.data;
   } catch (error) {
--- src/services/api/employee/RegisterEmployee.api.js
+++ src/services/api/employee/RegisterEmployee.api.js
@@ -1,4 +1,4 @@
-import http from "../../Interceptor/Interceptor";
+import http from '../../Interceptor/Interceptor';
 
 //Main Url Of Our Project Backend
 const MainURL = process.env.REACT_APP_PUBLIC_API_URL;
@@ -6,7 +6,10 @@ const MainURL = process.env.REACT_APP_PUBLIC_API_URL;
 export const RegisterEmployee = async (object) => {
   try {
     //Calling Api To Send The Information From Register Form
-    const result = await http.post(`${MainURL}auth/employee/register`, object);
+    const result = await http.post(
+      `${MainURL}auth/employee/register`,
+      object
+    );
 
     return result.data;
   } catch (error) {
